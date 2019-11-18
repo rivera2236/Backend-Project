271744eb1b4c0fcb69fb58c5049f89755d46164c
jorge rivera <rivera2236@gmail.com>
2019-11-18 09:14:09 -0500

    before destroying test branch

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+
+// require("bootstrap-sass");  //NOT SURE IF I'LL NEED THIS???
